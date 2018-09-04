9b87c18e47022afbb87cbe60e967940e376c45d2
lihawl@126.com <lihawl@126.com>
2018-09-04 13:00:39 +0800

    perturbation split

--- QuPerturbation1DPrecision/QuPerturbation1DPrecision.cpp
+++ QuPerturbation1DPrecision/QuPerturbation1DPrecision.cpp
@@ -6,7 +6,7 @@ int main()
 	QuPerturbation1D per;
 	per.init(FunctorWrapper("0.0001*exp(-x*x)"), -5000, 5000, 200000, 0.5, 0.001,
 		1, SolverMethod::BornSerise, 1, 1, std::map<std::string, std::string>());
-	per.Compute();
+
 	printf("Max MOmentum %lf\n", per.GetMaxMomentum());
 	printf("Max Energy %lf\n", per.GetMaxEnergy());
 
@@ -18,13 +18,14 @@ int main()
 
 	printf("Momentum Gap / Epsilon Momentum Width %lf\n", per.GetMomentumGap() / per.GetEpsilonMomentumWidth());
 	printf("Energy Gap / Epsilon %lf\n", per.GetEnergyGap() / per.GetEpsilon());
-	printf("Epsilon Boundary Error %lf", per.GetEpsilonBoundaryError());
+	printf("Epsilon Boundary Error %lf\n", per.GetEpsilonBoundaryError());
 
+	per.Compute();
 	printf("R %lf\n", per.GetR());
 
 
 	printf("%10s | %10s %10s %10s %10s | %10s\n", "V0", "RO1", "RO2", "RO10", "RO1N3", "Exact");
-	for (int i = 0; i < 20; ++i) {
+	for (int i = 0; i < 50; ++i) {
 		
 		Real v0 = 0.01 + 0.05*i;
 		auto vfunc = [&](Real x) { return v0*exp(-x * x); };
@@ -54,8 +55,8 @@ int main()
 
 		QuPerturbation1D per4;
 		std::map<std::string, std::string> o4;
-		o4["order"] = "1";
-		o4["split_n"] = "3";
+		o4["order"] = "3";
+		o4["split_n"] = "4";
 		per4.init(vfunc, -5000, 5000, 200000, 0.5, 0.002,
 			1, SolverMethod::BornSerise, 1, 1, o4);
 		per4.Compute();
--- qsim/Perturbation1DImpl.cpp
+++ qsim/Perturbation1DImpl.cpp
@@ -80,6 +80,19 @@ void QuPerturbation1DImpl::Compute()
 				b[i] += a[i];
 			}
 		};
+
+		auto sub = [this](PsiVector const &a, PsiVector &b) {
+			for (size_t i = 0; i < fNx; ++i) {
+				b[i] -= a[i];
+			}
+		};
+
+		auto Scale = [this](Real f, PsiVector &a) {
+			for (size_t i = 0; i < fNx; ++i) {
+				a[i] *= f;
+			}
+		};
+
 		auto VProd = [this](PsiVector const &psix, PsiVector &retx) {
 			for (size_t i = 0; i < fNx; ++i) {
 				retx[i] = fV[i] * psix[i];
@@ -110,14 +123,89 @@ void QuPerturbation1DImpl::Compute()
 		if (fSplit == 1) {
 			for (int i = 0; i < fOrder; ++i) {
 				if (i == 0) {
+					// G psi0
 					VProd(fPsi0X, ftmp1);
+					X2K(ftmp1, ftmp2);
+					G0Prod(ftmp2, fPsiK);
+					K2X(fPsiK, fPsiX);
+				} else {
+					// O2: G ( 1 + G ) psi0 = G ( psi0 + O1)
+					// O3: G ( 1 + G ( 1 + G ) ) psi0 = G ( psi0 + O2)
+					add(fPsi0X, fPsiX);
+					VProd(fPsiX, ftmp1);
+					X2K(ftmp1, ftmp2);
+					G0Prod(ftmp2, fPsiK);
+					K2X(fPsiK, fPsiX);
+				}
+			}
+		} else {
+
+			size_t const NP = (int)pow(fOrder + 1, fSplit);
+			std::vector<Real> coef(NP);
+			std::vector<Real> ccoef(NP);
+			coef[0] = 1;
+			std::function<void(int n, int cur, std::vector<Real> const &coef, std::vector<Real> &ccoef)> CalCoef =
+				[&](int n, int cur, std::vector<Real> const &coef, std::vector<Real> &ccoef) {
+
+				if (cur == 0) {
+					ccoef = coef;
+					return;
+				}  
+				std::vector<Real> g(NP);
+				std::vector<Real> cg(NP);
+
+				g = coef;
+				for (int o = 0; o < fOrder; ++o) {
+					for (size_t i = coef.size()-1; i > 0; --i) {
+						g[i] = 1. / fSplit * g[i - 1];
+					}
+					g[0] = 0;
+					CalCoef(n, cur - 1, g, cg);
+					for (size_t i = 0; i < g.size(); ++i) {
+						g[i] = coef[i] + cg[i];
+					}
+				}
+
+				CalCoef(n, cur - 1, g, ccoef);
+
+
+			};
+			CalCoef(fSplit, fSplit, coef, ccoef);
+
+			if (0) {
+				for (size_t i = 0; i < ccoef.size(); ++i) {
+					printf("%lf\n", ccoef[i]);
+				}
+			}
+
+			for (size_t i = 1; i < ccoef.size()-1; ++i) {
+				for (size_t j = i+1; j < ccoef.size(); ++j) {
+					ccoef[j] /= ccoef[i];
+				}
+			}
+
+			if (0) {
+				for (size_t i = 0; i < ccoef.size(); ++i) {
+					printf("%lf\n", ccoef[i]);
+				}
+			}
+
+			for (size_t i = ccoef.size()-1; i > 0; --i) {
+
+				if (i == ccoef.size() - 1) {
+					VProd(fPsi0X, ftmp1);
+					Scale(ccoef[i], ftmp1);
+					X2K(ftmp1, ftmp2);
+					G0Prod(ftmp2, fPsiK);
+					K2X(fPsiK, fPsiX);
 				} else {
 					add(fPsi0X, fPsiX);
 					VProd(fPsiX, ftmp1);
+					Scale(ccoef[i], ftmp1);
+					X2K(ftmp1, ftmp2);
+					G0Prod(ftmp2, fPsiK);
+					K2X(fPsiK, fPsiX);
 				}
-				X2K(ftmp1, ftmp2);
-				G0Prod(ftmp2, fPsiK);
-				K2X(fPsiK, fPsiX);
 			}
 
 		}
