00b88cd83591c7d171a38eebdffcfdbda281b4b8
lihawl@126.com <lihawl@126.com>
2018-09-19 21:09:22 +0800

    test added

---
+++ QuScattering3DPrecision/QuScattering3DPrecision.cpp
@@ -0,0 +1,71 @@
+#include "../qsim/QuSim.h"
+
+double born(double mass, double hbar, double p,
+	double v0, double alpha,
+	double cosx, double cosy, double cosz)
+{
+	double kx = cosx *p / hbar;
+	double ky = cosy * p / hbar;
+	double kz = (cosz-1) * p / hbar;
+	double mat = v0 * pow(Pi/alpha, 1.5) * exp(-(kx*kx + ky*ky + kz*kz) / (4 * alpha));
+	return mass * mass / (2*Pi*2*Pi*hbar*hbar*hbar*hbar) * mat*mat;
+
+}
+
+void test0()
+{
+	std::map<std::string, std::string> opts;
+	QuScatteringInverseMatrix3D solver;
+	Real p = 1;
+	Real e = p * p / 2;
+	solver.init([](Real x, Real y, Real z) { return 0.01*exp(-(x*x + y * y + z * z)); },
+		-10, 10, 100, -10, 10, 100, -10, 10, 100,
+		e, 0, 0, 1, SolverMethod::MatrixInverse, 1, 1, opts);
+
+	//solver.Compute();
+
+	for (int i = 0; i < 11; ++i) {
+		Real theta = (0.0 + i * 0.1) * Pi;
+		Real cosx = sin(theta);
+		Real cosy = 0;
+		Real cosz = cos(theta);
+		Real a = solver.ComputeXSection(cosx, cosy, cosz);
+		Real b = born(1, 1, p, 0.01, 1, cosx, cosy, cosz);
+		printf("%5.3E | %10.5E %10.5E %10.5E\n", theta, a, b, (a-b)/b);
+	}
+
+}
+
+
+void test1()
+{
+	std::map<std::string, std::string> opts;
+	opts["matrix_solver"] = "BiCGSTAB";
+	//opts["preconditioner"] = "DiagonalPreconditioner";
+	opts["preconditioner"] = "IdentityPreconditioner";
+	QuScatteringInverseMatrix3D solver;
+	Real p = 1;
+	Real e = p * p / 2;
+	solver.init([](Real x, Real y, Real z) { return 0.01*exp(-(x*x + y * y + z * z)); },
+		-25, 25, 100, -25, 25, 100, -25, 25, 100,
+		e, 0, 0, 1, SolverMethod::MatrixInverse, 1, 1, opts);
+
+	solver.Compute();
+
+	for (int i = 0; i < 11; ++i) {
+		Real theta = (0.0 + i * 0.1) * Pi;
+		Real cosx = sin(theta);
+		Real cosy = 0;
+		Real cosz = cos(theta);
+		Real a = solver.ComputeXSection(cosx, cosy, cosz);
+		Real b = born(1, 1, p, 0.01, 1, cosx, cosy, cosz);
+		printf("%5.3E | %10.5E %10.5E %10.5E\n", theta, a, b, (a - b) / b);
+	}
+
+}
+
+int main()
+{
+	test0();
+	test1();
+}
