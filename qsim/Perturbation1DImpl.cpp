894c3217f6ccb03e86be8da1a802da9db5dd003f
lihawl@126.com <lihawl@126.com>
2018-09-24 13:46:53 +0800

    very refine codes for scattering perburbation 1d

--- qsim/Perturbation1DImpl.cpp
+++ qsim/Perturbation1DImpl.cpp
@@ -90,7 +90,51 @@ void QuPerturbation1DImpl::InitPerturbation1D(std::function<Complex(Real)> const
 void QuPerturbation1DImpl::Compute()
 {
 
-	if (fMet == SolverMethod::BornSerise) {
+	auto X2K = [this](Complex const *psix, Complex *psik) {
+		fFFT->Transform(psix, psik);
+	};
+
+	auto K2X = [this](Complex const *psik, Complex *psix) {
+		fInvFFT->Transform(psik, psix);
+		for (size_t i = 0; i < fNx; ++i) {
+			psix[i] *= 1. / (fNx);
+		}
+	};
+
+
+	if (fPreconditional) { // Preconditional Born serise
+
+
+		std::vector<Real> imV(fNx);
+		{
+			for (size_t i = 0; i < fNx; ++i) {
+				Real lambda = 2 * Pi / (sqrt(2 * fE*fMass) / fHbar);
+				Real x = GetX(i);
+				if (i < fNx / 2) {
+					Real xx = (x - fX0) / (4 * lambda);
+					imV[i] = -fE * exp(-xx * xx);
+				} else {
+					Real xx = (x - (fX0 + fDx * fNx)) / (4 * lambda);
+					imV[i] = -fE * exp(-xx * xx);
+				}
+			}
+		};
+
+
+		PreconditionalBornSerise pbs;
+		Real minEpsilon = pbs.GetMinEpsilon(fNx, fV.data(), imV.data());
+		Real const epsilon = (fEpsilon < minEpsilon ? minEpsilon : fEpsilon);
+
+		ftmp1.resize(fNx);
+
+
+		for (int i = 0; i < fOrder; ++i) {
+			pbs.Update1D(fNx, fPsi0X.data(), fPsiX.data(), fPsiK.data(),
+				fV.data(), imV.data(), ftmp1.data(), epsilon, fE,
+				fMass, fHbar, fDx, X2K, K2X, fSlow, fPreconditioner);
+		}
+
+	} else if (fSplit != 0 && fSplit != 0) { // n-split Born serise
 
 		auto add = [this](PsiVector const &a, PsiVector &b) {
 			for (size_t i = 0; i < fNx; ++i) {
@@ -122,17 +166,6 @@ void QuPerturbation1DImpl::Compute()
 			}
 		};
 
-		auto G0Prod = [this](PsiVector const &psik, PsiVector &retk) {
-			for (size_t i = 0; i < fNx; ++i) {
-				ptrdiff_t ii = i < fNx / 2 ? i : fNx - i;
-				Real p = 2 * Pi / (fNx * fDx) * ii * fHbar;
-				Real e = p * p / (2 * fMass);
-				Complex Green0K = 1. / (fE + I * fEpsilon - e);
-				retk[i] = Green0K * psik[i];
-			}
-
-		};
-
 		auto G0ProdU = [this](PsiVector &psik) {
 			for (size_t i = 0; i < fNx; ++i) {
 				ptrdiff_t ii = i < fNx / 2 ? i : fNx - i;
@@ -144,187 +177,119 @@ void QuPerturbation1DImpl::Compute()
 
 		};
 
-		auto G0ProdEpsilonU = [this](PsiVector &psik, Real epsilon) {
-			for (size_t i = 0; i < fNx; ++i) {
-				ptrdiff_t ii = i < fNx / 2 ? i : fNx - i;
-				Real p = 2 * Pi / (fNx * fDx) * ii * fHbar;
-				Real e = p * p / (2 * fMass);
-				Complex Green0K = 1. / (fE + I * epsilon - e);
-				psik[i] = Green0K * psik[i];
+		size_t const NP = (int)pow(fOrder + 1, fSplit);
+		std::vector<Real> coef(NP);
+		std::vector<Real> ccoef(NP);
+		coef[0] = 1;
+		std::function<void(int n, int cur, std::vector<Real> const &coef, std::vector<Real> &ccoef)> CalCoef =
+			[&](int n, int cur, std::vector<Real> const &coef, std::vector<Real> &ccoef) {
+
+			if (cur == 0) {
+				ccoef = coef;
+				return;
+			}
+			std::vector<Real> g(NP);
+			std::vector<Real> cg(NP);
+
+			g = coef;
+			for (int o = 0; o < fOrder; ++o) {
+				for (size_t i = coef.size() - 1; i > 0; --i) {
+					g[i] = 1. / fSplit * g[i - 1];
+				}
+				g[0] = 0;
+				CalCoef(n, cur - 1, g, cg);
+				for (size_t i = 0; i < g.size(); ++i) {
+					g[i] = coef[i] + cg[i];
+				}
 			}
 
+			CalCoef(n, cur - 1, g, ccoef);
+
+
+		};
+		CalCoef(fSplit, fSplit, coef, ccoef);
+
+		if (0) {
+			for (size_t i = 0; i < ccoef.size(); ++i) {
+				printf("%lf\n", ccoef[i]);
+			}
+		}
+
+		for (size_t i = 1; i < ccoef.size() - 1; ++i) {
+			for (size_t j = i + 1; j < ccoef.size(); ++j) {
+				ccoef[j] /= ccoef[i];
+			}
+		}
+
+		if (0) {
+			for (size_t i = 0; i < ccoef.size(); ++i) {
+				printf("%lf\n", ccoef[i]);
+			}
+		}
+
+		for (size_t i = ccoef.size() - 1; i > 0; --i) {
+
+			if (i == ccoef.size() - 1) {
+				VProd(fPsi0X, fPsiX);
+			} else {
+				add(fPsi0X, fPsiX);
+				VProdU(fPsiX);
+			}
+			Scale(ccoef[i], fPsiX);
+			X2K(fPsiX.data(), fPsiK.data());
+			G0ProdU(fPsiK);
+			K2X(fPsiK.data(), fPsiX.data());
+		}
+
+	} else { // naive born serise
+		BornSerise bs;
+
+		auto X2K = [this](Complex const *psix, Complex *psik) {
+			fFFT->Transform(psix, psik);
 		};
 
-		auto X2K = [this](PsiVector const &psix, PsiVector &psik) {
-			fFFT->Transform(psix.data(), psik.data());
-		};
-
-		auto K2X = [this](PsiVector const &psik, PsiVector &psix) {
-			fInvFFT->Transform(psik.data(), psix.data());
+		auto K2X = [this](Complex const *psik, Complex *psix) {
+			fInvFFT->Transform(psik, psix);
 			for (size_t i = 0; i < fNx; ++i) {
 				psix[i] *= 1. / (fNx);
 			}
 		};
 
-		if (fPreconditional) { // Preconditional Born serise
-			
-
-			std::vector<Real> imV(fNx);
-			{
-				for (size_t i = 0; i < fNx; ++i) {
-					Real lambda = 2 * Pi / (sqrt(2 * fE*fMass) / fHbar);
-					Real x = GetX(i);
-					if (i < fNx / 2) {
-						Real xx = (x - fX0) / (4 * lambda);
-						imV[i] = -fE * exp(-xx * xx);
-					} else {
-						Real xx = (x - (fX0 + fDx * fNx)) / (4 * lambda);
-						imV[i] = -fE * exp(-xx * xx);
-					}
-				}
-			};
+		for (int i = 0; i < fOrder; ++i) {
+			bs.Update1D(fNx, fPsi0X.data(), fPsiX.data(),
+				fPsiK.data(), fV.data(), fEpsilon, fE, fMass, fHbar, fDx,
+				X2K, K2X);
+		}
+	}
 
 
-			PreconditionalBornSerise pbs;
-			Real minEpsilon = pbs.GetMinEpsilon(fNx, fV.data(), imV.data());
-			Real const epsilon = (fEpsilon < minEpsilon ? minEpsilon : fEpsilon);
-
-			auto X2K = [this](Complex const *psix, Complex *psik) {
-				fFFT->Transform(psix, psik);
-			};
-
-			auto K2X = [this](Complex const *psik, Complex *psix) {
-				fInvFFT->Transform(psik, psix);
-				for (size_t i = 0; i < fNx; ++i) {
-					psix[i] *= 1. / (fNx);
-				}
-			};
-
-			ftmp1.resize(fNx);
-
-
-			for (int i = 0; i < fOrder; ++i) {
-				pbs.Update1D(fNx, fPsi0X.data(), fPsiX.data(), fPsiK.data(),
-					fV.data(), imV.data(), ftmp1.data(), epsilon, fE,
-					fMass, fHbar, fDx, X2K, K2X, fSlow, fPreconditioner);
-			}
-
-		} else if (fSplit != 0 && fSplit != 0) { // n-split Born serise
-
-			size_t const NP = (int)pow(fOrder + 1, fSplit);
-			std::vector<Real> coef(NP);
-			std::vector<Real> ccoef(NP);
-			coef[0] = 1;
-			std::function<void(int n, int cur, std::vector<Real> const &coef, std::vector<Real> &ccoef)> CalCoef =
-				[&](int n, int cur, std::vector<Real> const &coef, std::vector<Real> &ccoef) {
-
-				if (cur == 0) {
-					ccoef = coef;
-					return;
-				}  
-				std::vector<Real> g(NP);
-				std::vector<Real> cg(NP);
-
-				g = coef;
-				for (int o = 0; o < fOrder; ++o) {
-					for (size_t i = coef.size()-1; i > 0; --i) {
-						g[i] = 1. / fSplit * g[i - 1];
-					}
-					g[0] = 0;
-					CalCoef(n, cur - 1, g, cg);
-					for (size_t i = 0; i < g.size(); ++i) {
-						g[i] = coef[i] + cg[i];
-					}
-				}
-
-				CalCoef(n, cur - 1, g, ccoef);
-
-
-			};
-			CalCoef(fSplit, fSplit, coef, ccoef);
-
-			if (0) {
-				for (size_t i = 0; i < ccoef.size(); ++i) {
-					printf("%lf\n", ccoef[i]);
-				}
-			}
-
-			for (size_t i = 1; i < ccoef.size()-1; ++i) {
-				for (size_t j = i+1; j < ccoef.size(); ++j) {
-					ccoef[j] /= ccoef[i];
-				}
-			}
-
-			if (0) {
-				for (size_t i = 0; i < ccoef.size(); ++i) {
-					printf("%lf\n", ccoef[i]);
-				}
-			}
-
-			for (size_t i = ccoef.size()-1; i > 0; --i) {
-
-				if (i == ccoef.size() - 1) {
-					VProd(fPsi0X, fPsiX);
-				} else {
-					add(fPsi0X, fPsiX);
-					VProdU(fPsiX);
-				}
-				Scale(ccoef[i], fPsiX);
-				X2K(fPsiX, fPsiK);
-				G0ProdU(fPsiK);
-				K2X(fPsiK, fPsiX);
-			}
-
-		} else { // naive born serise
-			BornSerise bs;
-
-			auto X2K = [this](Complex const *psix, Complex *psik) {
-				fFFT->Transform(psix, psik);
-			};
-
-			auto K2X = [this](Complex const *psik, Complex *psix) {
-				fInvFFT->Transform(psik, psix);
-				for (size_t i = 0; i < fNx; ++i) {
-					psix[i] *= 1. / (fNx);
-				}
-			};
-
-			for (int i = 0; i < fOrder; ++i) {
-				bs.Update1D(fNx, fPsi0X.data(), fPsiX.data(),
-					fPsiK.data(), fV.data(), fEpsilon, fE, fMass, fHbar, fDx,
-					X2K, K2X);
-			}
+	// post calculation
+	if (0) {
+		// calculate in momentum space
+		Real r = 0;
+		Real t = 0;
+		for (size_t i = fNx / 2; i < fNx; ++i) {
+			r += abs2(fPsiK[i]);
 		}
 
-
-		// post calculation
-		if (0) {
-			// calculate in momentum space
-			Real r = 0;
-			Real t = 0;
-			for (size_t i = fNx / 2; i < fNx; ++i) {
-				r += abs2(fPsiK[i]);
-			}
-
-			for (size_t i = 0; i < fNx / 2; ++i) {
-				t += abs2(fPsiK[i]);
-			}
-
-			fR = r * fEpsilon / fE * fDx / fNx;
-			fT = t * fEpsilon / fE * fDx / fNx;
-
-		} else {
-			// calculate in real space
-			Complex r = 0;
-			Complex t = 0;
-			for (size_t i = 0; i < fNx; ++i) {
-				r += (fPsi0X[i] + fPsiX[i])*fV[i] * exp(+I * fK0*GetX(i));
-				t += (fPsi0X[i] + fPsiX[i])*fV[i] * exp(-I * fK0*GetX(i));
-			}
-			fR = abs2(r*fDx*fMass / (fHbar*fHbar*fK0 * I));
-			fT = abs2(t*fDx*fMass / (fHbar*fHbar*fK0 * I));
-
+		for (size_t i = 0; i < fNx / 2; ++i) {
+			t += abs2(fPsiK[i]);
 		}
+
+		fR = r * fEpsilon / fE * fDx / fNx;
+		fT = t * fEpsilon / fE * fDx / fNx;
+
+	} else {
+		// calculate in real space
+		Complex r = 0;
+		Complex t = 0;
+		for (size_t i = 0; i < fNx; ++i) {
+			r += (fPsi0X[i] + fPsiX[i])*fV[i] * exp(+I * fK0*GetX(i));
+			t += (fPsi0X[i] + fPsiX[i])*fV[i] * exp(-I * fK0*GetX(i));
+		}
+		fR = abs2(r*fDx*fMass / (fHbar*fHbar*fK0 * I));
+		fT = abs2(t*fDx*fMass / (fHbar*fHbar*fK0 * I));
+
 	}
 
 }
